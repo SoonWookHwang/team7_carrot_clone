441a073144bfb1f038b8a06d89062cde7482d712
choimyb <moungbak421@daum.net>
2022-08-20 14:06:06 +0900

    WebSecurityConfig has added & PasswordEncoder has added

---
+++ src/main/java/com/innovation/team7_carrot_clone/security/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.innovation.team7_carrot_clone.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity // 스프링 Security 지원을 가능하게 함
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+// 어떤 요청이든 '인증'
+                .anyRequest().authenticated()
+                .and()
+// 로그인 기능 허용
+                .formLogin()
+                .defaultSuccessUrl("/")
+                .permitAll()
+                .and()
+// 로그아웃 기능 허용
+                .logout()
+                .permitAll();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
