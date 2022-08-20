b36f7ca41fec63fd1905a3ef8a5d9cf733762419
choimyb <moungbak421@daum.net>
2022-08-20 11:03:17 +0900

    TEST : Testing DB Working

--- src/main/java/com/innovation/team7_carrot_clone/Team7CarrotCloneApplication.java
+++ src/main/java/com/innovation/team7_carrot_clone/Team7CarrotCloneApplication.java
@@ -1,8 +1,16 @@
 package com.innovation.team7_carrot_clone;
 
+import com.innovation.team7_carrot_clone.model.Post;
+import com.innovation.team7_carrot_clone.repository.PostRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.List;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class Team7CarrotCloneApplication {
 
@@ -10,4 +18,19 @@ public class Team7CarrotCloneApplication {
 		SpringApplication.run(Team7CarrotCloneApplication.class, args);
 	}
 
+	@Bean
+		public CommandLineRunner demo(PostRepository postRepository) {
+			return (args) -> {
+				Post post = new Post("전자렌지 팔아요", "사용한지 6개월 된 거의 새 제품 입니다.", "전자/가전", "25000원", "/images/제목없음.png");
+				postRepository.save(post);
+				List<Post> postList = postRepository.findAll(); // 조회
+				for(Post p : postList){
+					System.out.println(p.getTitle());
+					System.out.println(p.getContents());
+					System.out.println(p.getCategory());
+					System.out.println(p.getPrice());
+					System.out.println(p.getImgURL());
+				}
+			};
+		}
 }
