d53cfd8b802f311227797d5ae36048d697b91350
kwanghunlee <lkhun9311@naver.com>
2022-08-23 13:54:58 +0900

    Add AWS S3 파일 업로드 관련 코드 추가, AWS RDS의 MySQL과 연동하므로 사용하지 않는 코드 주석 처리.

--- src/main/java/com/innovation/team7_carrot_clone/Team7CarrotCloneApplication.java
+++ src/main/java/com/innovation/team7_carrot_clone/Team7CarrotCloneApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import java.util.List;
 
@@ -19,18 +20,26 @@ public class Team7CarrotCloneApplication {
 	}
 
 	@Bean
-		public CommandLineRunner demo(PostRepository postRepository) {
-			return (args) -> {
-				Post post = new Post("전자렌지 팔아요", "사용한지 6개월 된 거의 새 제품 입니다.", "전자/가전", "25000원", "/images/제목없음.png");
-				postRepository.save(post);
-				List<Post> postList = postRepository.findAll(); // 조회
-				for(Post p : postList){
-					System.out.println(p.getTitle());
-					System.out.println(p.getContents());
-					System.out.println(p.getCategory());
-					System.out.println(p.getPrice());
-					System.out.println(p.getImgURL());
-				}
-			};
-		}
+	public CommonsMultipartResolver multipartResolver() {
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+		multipartResolver.setDefaultEncoding("UTF-8"); // 파일 인코딩 설정
+		multipartResolver.setMaxUploadSizePerFile(5 * 1024 * 1024); // 파일당 업로드 크기 제한 (5MB)
+		return multipartResolver;
+	}
+
+//	@Bean
+//		public CommandLineRunner demo(PostRepository postRepository) {
+//			return (args) -> {
+//				Post post = new Post("전자렌지 팔아요", "사용한지 6개월 된 거의 새 제품 입니다.", "전자/가전", "25000원", "/images/제목없음.png");
+//				postRepository.save(post);
+//				List<Post> postList = postRepository.findAll(); // 조회
+//				for(Post p : postList){
+//					System.out.println(p.getTitle());
+//					System.out.println(p.getContents());
+//					System.out.println(p.getCategory());
+//					System.out.println(p.getPrice());
+//					System.out.println(p.getImageUrl());
+//				}
+//			};
+//		}
 }
