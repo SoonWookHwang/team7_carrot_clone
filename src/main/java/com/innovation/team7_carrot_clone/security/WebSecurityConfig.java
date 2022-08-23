0570a2076e6f7f1777de3329cede9e00a40d2505
kwanghunlee <lkhun9311@naver.com>
2022-08-23 19:11:49 +0900

    Fix WebSecurityConfig 설정 변경.

--- src/main/java/com/innovation/team7_carrot_clone/security/WebSecurityConfig.java
+++ src/main/java/com/innovation/team7_carrot_clone/security/WebSecurityConfig.java
@@ -7,77 +7,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
-//import com.innovation.team7_carrot_clone.jwt.JwtTokenProvider;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.builders.WebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.util.matcher.RequestMatcher;
-//
-//@Configuration
-//@EnableWebSecurity // 스프링 Security 지원을 가능하게 함
-//@EnableGlobalMethodSecurity(securedEnabled = true)
-//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//    private final JwtTokenProvider jwtTokenProvider;
-//
-//    @Bean
-//    public AuthenticationManager authenticationManagerBean() throws Exception {
-//        return super.authenticationManagerBean();
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder(){
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    public void configure(WebSecurity web) {
-//        web.ignoring().requestMatchers(new RequestMatcher[]{PathRequest.toStaticResources().atCommonLocations()});
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//
-//        http.authorizeRequests()
-//// 어떤 요청이든 '인증'
-//                .anyRequest().authenticated()
-//                .and()
-//// 로그인 기능 허용
-//                .formLogin()
-//                .defaultSuccessUrl("/")
-//                .permitAll()
-//                .and()
-//// 로그아웃 기능 허용
-//                .logout()
-//                .permitAll();
-//    }
-//
-//    public WebSecurityConfig(JwtTokenProvider jwtTokenProvider) {
-//        this.jwtTokenProvider = jwtTokenProvider;
-//    }
-//}
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(
         securedEnabled = true
 )
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
     private final JwtTokenProvider jwtTokenProvider;
 
     @Bean
@@ -86,32 +32,37 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
+    // (기본설정) 스프링부트가 제공하는 static 리소스들의 기본위치를 가져와 스프링 시큐리티에서 제외
     public void configure(WebSecurity web) {
-//        web.ignoring().requestMatchers(new RequestMatcher[]{PathRequest.toStaticResources().atCommonLocations()});
+        web.ignoring().mvcMatchers("/index.html");
         web.ignoring().requestMatchers(new RequestMatcher[]{PathRequest.toStaticResources().atCommonLocations()});
     }
 
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+        http.cors();
         http.headers().frameOptions().disable();
-        ((HttpSecurity) ((HttpSecurity) ((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl)
-                ((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl)
-                        ((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl)
-                                ((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl)
-                                        ((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl)
-                                                http.authorizeRequests().antMatchers(new String[]{"/user/**"})).permitAll()
-                                                .antMatchers(new String[]{"**"})).permitAll()
-                                        .antMatchers(new String[]{"/"})).permitAll()
-                                .antMatchers(HttpMethod.GET, new String[]{"/api/posts"}))
-                        .permitAll().anyRequest()).authenticated()
-                .and())
+        http.authorizeRequests()
+                .mvcMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .antMatchers("/api/**").permitAll()
+                .antMatchers("/js/**").permitAll()
+                .antMatchers("/scripts/**").permitAll()
+                .antMatchers("/css/**").permitAll()
+                .antMatchers("/users/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                // 세션인증 사용하지 않음
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and())
-                .addFilterBefore(new JwtAuthenticationFilter(this.jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+                .and()
+                // 토큰인증 먼저 실행
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+                        UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
 
     public WebSecurityConfig(JwtTokenProvider jwtTokenProvider) {
